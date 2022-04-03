35aa4cec6c1f6fde6cb9d6c26fc18b07395928aa
iamagf <61362029+iam-agf@users.noreply.github.com>
2022-04-02 19:11:12 -0600

    Removed notes

--- server.js
+++ server.js
@@ -14,18 +14,6 @@ app.use(helmet.expectCt());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 app.use(helmet.xssFilter());
-// app.use(helmet.contentSecurityPolicy({
-//     directives: {
-//       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-//       "script-src": ["'unsafe-eval'","'self'", 	"antoniogf.com", "polyfill.io","fonts.googleapis.com"],
-// 	  "data": ["'unsafe-eval'","'self'", 	"antoniogf.com", "polyfill.io","fonts.googleapis.com"],
-// 	  "font-src":["'unsafe-eval'","'self'", 	"antoniogf.com","fonts.googleapis.com", 'https://fonts.gstatic.com'],
-// 	  "connect-src":["'unsafe-eval'","'self'", 	"antoniogf.com","fonts.googleapis.com", 'https://fonts.gstatic.com'],
-	  
-//     },
-//   })
-// );
-
 
 app.use(function(req,res){
 	res.sendFile(path.resolve('./src/app.html'));
