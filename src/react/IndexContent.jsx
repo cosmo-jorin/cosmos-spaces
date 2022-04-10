8508ac3b99937b6a804665335e3787c2edb10371
iamagf <61362029+iam-agf@users.noreply.github.com>
2022-04-10 02:13:27 -0500

    Css details and shooting stars

--- src/react/IndexContent.jsx
+++ src/react/IndexContent.jsx
@@ -5,6 +5,14 @@ class IndexContent extends React.Component {
   render() {
     return (
       <div className="index-content">
+        <div className="sky" style={{transform: "rotate(45deg)"}}>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+          <div className="star"></div>
+        </div>
         <div className="index-main-block">
             <div>
               <h1>Title</h1>
--- src/react/TitleBar.jsx
+++ src/react/TitleBar.jsx
@@ -14,8 +14,12 @@ class TitleBar extends React.Component {
 
         <div id="right">
           <ul id="title-bar-menu">
-            <li><Link to='/Information'><button>Cosmos information</button></Link></li>
-            <li><Link to='/AboutUs'><button>About us</button></Link></li>
+          <li><Link to='/'><button>Home</button></Link></li>
+            <li><Link to='/AboutUs'><button>Spaces Team</button></Link></li>
+            <li><Link to='/contact'><button>Validators</button></Link></li>
+            <li><Link to='/Information'><button>Networks</button></Link></li>
+            <li><Link to='/AboutUs'><button>Links</button></Link></li>
+            <li><Link to='/Information'><button>Airdrops</button></Link></li>
             <li><Link to='/contact'><button>Contact</button></Link></li>
           </ul>
         </div>
