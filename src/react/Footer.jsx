ca7df0449d11b1c33808b96e8f38abcf38c9552c
iamagf <102500749+cosmo-jorin@users.noreply.github.com>
2022-04-30 23:03:35 -0500

    Footer edit

--- src/react/Footer.jsx
+++ src/react/Footer.jsx
@@ -6,8 +6,19 @@ class Footer extends React.Component{
   render(){
     return(
       <div className='footer'>
-            <p><Link to="Contact">Contact</Link></p>
-      </div>
+        <a href="https://cosmosspaces.medium.com/"> 
+          <img src='/static/img/social/medium.svg' />
+        </a>
+        <a href="https://discord.com/invite/J8u7VPFc3T"> 
+          <img src='/static/img/social/discord.svg' />
+        </a>
+        <a href="https://t.me/cosmostwitterspaces"> 
+          <img src='/static/img/social/telegram.svg' />
+        </a>
+        <a href="https://twitter.com/Cosmos_Spaces"> 
+          <img src='/static/img/social/twitter.svg' />
+        </a>
+    </div>
     );
   }
 
