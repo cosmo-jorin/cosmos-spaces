55e2f6c7832944f8c284dda3c80f4d9355a6e6c0
iamagf <102500749+cosmo-jorin@users.noreply.github.com>
2022-05-02 20:56:49 -0500

    Images and blank addition

--- src/react/Footer.jsx
+++ src/react/Footer.jsx
@@ -6,16 +6,16 @@ class Footer extends React.Component{
   render(){
     return(
       <div className='footer'>
-        <a href="https://cosmosspaces.medium.com/"> 
+        <a href="https://cosmosspaces.medium.com/" target="_blank"> 
           <img src='/static/img/social/medium.svg' />
         </a>
-        <a href="https://discord.com/invite/J8u7VPFc3T"> 
+        <a href="https://discord.com/invite/J8u7VPFc3T" target="_blank"> 
           <img src='/static/img/social/discord.svg' />
         </a>
-        <a href="https://t.me/cosmostwitterspaces"> 
+        <a href="https://t.me/cosmostwitterspaces" target="_blank"> 
           <img src='/static/img/social/telegram.svg' />
         </a>
-        <a href="https://twitter.com/Cosmos_Spaces"> 
+        <a href="https://twitter.com/Cosmos_Spaces" target="_blank"> 
           <img src='/static/img/social/twitter.svg' />
         </a>
     </div>
