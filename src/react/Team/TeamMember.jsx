b62734be0a4e7894b87de057195bae9848b6d9f9
iamagf <102500749+cosmo-jorin@users.noreply.github.com>
2022-05-03 23:47:33 -0500

    Added that goddam transition

--- src/react/Team/TeamMember.jsx
+++ src/react/Team/TeamMember.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { CSSTransition } from 'react-transition-group';
+import ReactDOM from 'react-dom';
+
 import {nickname,twitter,images,info} from "./TeamInformation.jsx";
 
 class TeamMember extends React.Component {
@@ -7,15 +10,18 @@ class TeamMember extends React.Component {
   }
 
   render() {
-    if (!this.props.show){
-      return null;
-    }
     var image = images[this.props.name];
     var nick = nickname[this.props.name];
     var information = info[this.props.name];
     var link = twitter[this.props.name];
     return (
-      <div className="team-member" onClick={this.props.onClose}>
+      <CSSTransition
+        in={this.props.show}
+        timeout={300}
+        unmountOnExit
+        classNames="team-member"
+      >
+      <div className={"team-member"} onClick={this.props.onClose}>
         <div className="team-member-body" onClick={e => e.stopPropagation()}>
           <a href={link} target="_blank">
             <img id={this.props.name} src={image} alt={this.props.name}/>
@@ -24,6 +30,7 @@ class TeamMember extends React.Component {
           <p>{information}</p>
         </div>
       </div>
+      </CSSTransition>
     );
   }
 }
